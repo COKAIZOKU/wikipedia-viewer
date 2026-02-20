77a6981b6160af72ae9eccbd244a66ff0d7fe514
pvvegar <proteicode@proton.me>
2026-02-20 01:02:46 -0300

    style: changed search button color and added icon

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,8 @@ import { IconArrowRight, IconSearch } from '@tabler/icons-react';
 
 export default function Home() {
   const [value, setValue] = useState('');
+  const [response, setResponse] = useState(null);
+  
   var url = "https://en.wikipedia.org/w/api.php"; 
   var params = new URLSearchParams({
     action: "query",
@@ -13,16 +15,31 @@ export default function Home() {
     srsearch: value,
     format: "json",
     origin: "*", 
-});
+  });
 
   const handleSubmit = () => {
     fetch(`${url}?${params}`)
     .then(function(response){return response.json();})
     .then(function(response) {
         console.log("API response:", response);
+        setResponse(response);
+
+        const ids = (response.query?.search || [])
+        .map(x => x.pageid)
+        .join("|");
+
+        if(!ids) return;
+        
+        return fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=extracts&pageids=${ids}&exintro=1&explaintext=1&format=json`)
+          .then(r => r.json())
+          .then(extractsResponse => {
+            console.log("Extracts response:", extractsResponse);
+            setExtracts(extractsResponse);
+          })
     })
     .catch(function(error){console.log(error);});
   }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="gap-5 flex w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -45,11 +62,13 @@ export default function Home() {
                 handleSubmit();
               }
             }}
+            leftSection={<IconSearch size={18} stroke={1.5} />}
             rightSection={
               <ActionIcon 
               size={32}
               radius="xl"
               variant="filled"
+              color="#212529"
               aria-label="Search"
               onClick={handleSubmit}
               >
@@ -58,14 +77,15 @@ export default function Home() {
             }
           />
         </div>
-        <div className="flex flex-col sm:flex-col">
+        {response?.query?.search?.map(item => (
+        <div key={item.pageid} className="flex flex-col sm:flex-col">
           <h2 className="max-w-xs text-2xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Title.
+            {item.title}
           </h2>
-          <p className="max-w-xs tracking-tight text-black dark:text-zinc-50">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          <p dangerouslySetInnerHTML={{ __html: item.snippet}} className="max-w-xs tracking-tight text-black dark:text-zinc-50">
           </p>
         </div>
+        ))}
       </main>
     </div>
   );
